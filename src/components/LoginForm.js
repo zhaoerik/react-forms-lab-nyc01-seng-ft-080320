4a658bd898be7d52ed565183a4a6027293c77117
Erik Zhao <erikzhao98@gmail.com>
2020-10-08 03:41:11 +0000

    Automatically backed up by Learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -28,7 +28,7 @@ class LoginForm extends React.Component {
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
